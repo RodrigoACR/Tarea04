df530e5d6cb2703162c7872585788b3d9b434401
RodrigoACR <rodrigo.castro.rojas@est.una.ac.cr>
2023-10-28 18:09:10 -0600

    Interfaz

--- components/Coments.vue
+++ components/Coments.vue
@@ -1,30 +1,30 @@
 <template>
-    <div>
+    <div class="p-2">
         <h3>Comentarios</h3>
     </div>
-  </template>
+</template>
   
   
-  <script>
-  export default {
+<script>
+export default {
     mounted() {
-      const scriptTag = document.createElement('SCRIPT')
-      scriptTag.setAttribute('src', 'https://utteranc.es/client.js')
-      scriptTag.setAttribute('repo', 'RodrigoACR/Tarea04')
-      scriptTag.setAttribute('issue-term', 'pathname')
-      scriptTag.setAttribute('label', 'Comment')
-      scriptTag.setAttribute('theme', 'github-light')
-      scriptTag.setAttribute('crossorigin', 'anonymous')
-      scriptTag.setAttribute('async', 'async')
-      this.$el.append(scriptTag)
+        const scriptTag = document.createElement('SCRIPT')
+        scriptTag.setAttribute('src', 'https://utteranc.es/client.js')
+        scriptTag.setAttribute('repo', 'RodrigoACR/Tarea04')
+        scriptTag.setAttribute('issue-term', 'pathname')
+        scriptTag.setAttribute('label', 'Comment')
+        scriptTag.setAttribute('theme', 'boxy-light')
+        scriptTag.setAttribute('crossorigin', 'anonymous')
+        scriptTag.setAttribute('async', 'async')
+        this.$el.append(scriptTag)
     },
-  }
-  </script>
+}
+</script>
   
-  <style>
-  .utterances {
+<style>
+.utterances {
     width: 100%;
     margin: 0 !important;
     max-width: 100%;
-  }
-  </style>
+}
+</style>
